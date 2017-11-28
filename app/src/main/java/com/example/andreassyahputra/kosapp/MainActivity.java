5d3b1eba28816aa6c168e896351f3edb05439dbf
Andore_AZU <andreassyahputra03@gmail.com>
2017-11-28 10:17:30 +0700

    First Commit

---
+++ app/src/main/java/com/example/andreassyahputra/kosapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.andreassyahputra.kosapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
